6b4152d852bca7097abb18a68393331feb45b2ed
miguelpiedade2007 <miguel.piedade2007@gmail.com>
2026-03-19 09:11:06 -0300

    adicionado título ao arquivo

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main() {
+    printf("\nChekpoint da aula do professor Erick Toshio Yammamoto\n");
     printf("\nRafael Zani Gizzi\n");
     printf("Miguel Piedade\n");
 }
